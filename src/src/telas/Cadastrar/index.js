6333afb940eeb06be97ca8cc37577fbb135408e5
Diego Fraga <90010560+diegovfm12@users.noreply.github.com>
2023-05-06 22:02:46 -0300

    index.js

--- src/src/telas/Cadastrar/index.js
+++ src/src/telas/Cadastrar/index.js
@@ -4,9 +4,6 @@ import { SafeAreaView, StatusBar, Text, StyleSheet, View, TextInput, TouchableOp
 
 import { useNavigation } from '@react-navigation/native';
 
-<<<<<<< Updated upstream
-export default function Cadastrar() {
-=======
 
 const DismissKeyboard = ({children}) => (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
@@ -16,9 +13,8 @@ const DismissKeyboard = ({children}) => (
 );
 
 export default function Cadastrar (){
->>>>>>> Stashed changes
 
-    const navigation = useNavigation();
+    const navigation = useNavigation()
 
     return (
         <DismissKeyboard>
@@ -36,35 +32,14 @@ export default function Cadastrar (){
         
 
             <View style={styles.containerLogo}>
-<<<<<<< Updated upstream
-                <Image
-                    source={require('../../assets/logo.png')}
-                    style={{ width: '100%' }}
-                    resizeMode='contain'
-                />
-=======
             <Image
             source={require('../../assets/imglogo.png')}
             style={{width: '100%'}}
             resizeMode='center'
             />
->>>>>>> Stashed changes
             </View>
-
+            
             <View style={styles.containerForm}>
-<<<<<<< Updated upstream
-                <Text style={styles.title}>Nome Completo</Text>
-                <TextInput
-                    placeholder="Digite seu nome"
-                    style={styles.input}
-                />
-
-                <Text style={styles.title}>Nº do Celular</Text>
-                <TextInput
-                    placeholder="Digite seu número de telefone celular"
-                    style={styles.input}
-                />
-=======
 
             <Text style={styles.title}>Nome Completo</Text>
             <TextInput
@@ -77,40 +52,13 @@ export default function Cadastrar (){
             placeholder="Digite seu número de telefone celular"
             style={styles.input}
             />
->>>>>>> Stashed changes
 
-                <Text style={styles.title}>E-mail</Text>
-                <TextInput
-                    placeholder="Digite seu e-mail"
-                    style={styles.input}
-                />
+            <Text style={styles.title}>E-mail</Text>
+            <TextInput
+            placeholder="Digite seu e-mail"
+            style={styles.input}
+            />
 
-<<<<<<< Updated upstream
-                <Text style={styles.title}>Confirmar E-mail</Text>
-                <TextInput
-                    placeholder="Digite seu e-mail"
-                    style={styles.input}
-                />
-
-                <Text style={styles.title}>Senha</Text>
-                <TextInput
-                    placeholder="Sua senha"
-                    style={styles.input}
-                />
-
-                <Text style={styles.title}>Confirmar Senha</Text>
-                <TextInput
-                    placeholder="Sua senha"
-                    style={styles.input}
-                />
-
-                <TouchableOpacity style={styles.button}
-                    onPress={() => navigation.navigate('ConfirmCadastro')}
-                >
-                    <Text style={styles.buttonText}> Continuar</Text>
-                </TouchableOpacity>
-            </View>
-=======
 
 
             <Text style={styles.title}>Senha</Text>
@@ -129,7 +77,6 @@ export default function Cadastrar (){
             </TouchableOpacity>
            </View> 
            
->>>>>>> Stashed changes
 
         
         </View>
@@ -141,21 +88,6 @@ export default function Cadastrar (){
 }
 
 const styles = StyleSheet.create({
-<<<<<<< Updated upstream
-    container: {
-        flex: 1,
-        backgroundColor: '#FFFFFF'
-
-    },
-    containerLogo: {
-        flex: 0.2,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    containerForm: {
-        flex: 1,
-        backgroundColor: '#FFFFFF',
-=======
     container:{
         flex:1,
         backgroundColor:'#4F99FC'
@@ -169,19 +101,13 @@ const styles = StyleSheet.create({
     containerForm:{
         flex:1,
         backgroundColor:'#4F99FC',
->>>>>>> Stashed changes
         borderTopLeftRadius: 25,
         borderTopRightRadius: 25,
         paddingStart: '5%',
         paddingEnd: '5%',
     },
-<<<<<<< Updated upstream
-    title: {
-        marginTop: 28,
-=======
     title:{
         marginTop: 15,
->>>>>>> Stashed changes
         marginBottom: 12,
         fontSize: 15,
         fontWeight: "bold",
@@ -192,45 +118,29 @@ const styles = StyleSheet.create({
         
         
     },
-    input: {
+    input:{
         borderColor: '#e8e8e8',
-<<<<<<< Updated upstream
-        borderWidth: 1,
-        borderRadius: 5,
-=======
         borderWidth: 3,
         borderRadius:10,
->>>>>>> Stashed changes
         height: 40,
         marginBottom: 4,
         paddingHorizontal: 10,
         fontSize: 16,
         
     },
-<<<<<<< Updated upstream
-    button: {
-        backgroundColor: '#666666',
-        borderRadius: 4,
-=======
     button:{
         backgroundColor: 'white',
         borderRadius: 10,
->>>>>>> Stashed changes
         paddingVertical: 8,
         width: '40%',
         justifyContent: 'center',
-<<<<<<< Updated upstream
-        alignItems: 'center',
-        marginTop: 14
-=======
         alignItems:'center',
         marginLeft: 200,
         marginTop: 20,
         position:'relative'
         
->>>>>>> Stashed changes
     },
-    buttonText: {
+    buttonText:{
         fontSize: 18,
         color: 'black',
         fontWeight: 'bold',
