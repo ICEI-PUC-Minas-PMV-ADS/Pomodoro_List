3805a15a23f987276c6ffb66e5f1da094f868a1c
Vicinius115 <viciniuseduardo887@gmail.com>
2023-05-31 20:33:43 -0300

    att do react + criação db tarefas

---
+++ src/taskit/src/services/tarefas.services.js
@@ -0,0 +1,37 @@
+import API from './webapi.services';
+import {BASE_URL} from './urls';
+
+export const registerTarefa = async (param) => {
+  try{
+    return await API.post(`${BASE_URL}/tarefa`, param).then( 
+      response => {
+        return response.data;
+      },
+      error =>{
+        console.log(error);
+        return  null;
+      }
+    );
+  }catch(error){
+    console.log(error);
+    return null;
+  }
+
+}
+ export const getTarefas = async () => {
+    try{
+      return await API.get(`${BASE_URL}/tarefa`).then( 
+        response => {
+          const message = response.data?"funcionou":"não funcionou";
+          return { data: response.data, message: message };
+        },
+        error =>{
+          console.log(error);
+          return  null;
+        }
+      );
+    }catch(error){
+      console.log(error);
+      return null;
+    }
+}
--- src/taskit/src/services/urls.js
+++ src/taskit/src/services/urls.js
@@ -1,1 +1,1 @@
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'http://192.168.0.110:3000';
